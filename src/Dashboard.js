b93da4be72ec1388faf6d548c7fca15b0705193d
Rubens Stahl Júnior <rubens.stahl@serges.org>
2025-08-15 17:41:46 -0300

    atualização fopam 05

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -51,6 +51,7 @@ const monthsList = [
   { value: '2025-08', label: 'Agosto 2025' },
   { value: '2025-07', label: 'Julho 2025' },
   { value: '2025-06', label: 'Junho 2025' },
+  { value: '2025-05', label: 'Maio 2025' },
 ];
 
 export default function Dashboard({ userEmail, userName }) {
